6f328ba8a5986cbd285be491756f49c9e83bed0a
Camila Natali <natali.camila68@gmail.com>
2024-05-10 21:42:34 +0200

    Mise à jour du code

--- wp-content/themes/blocksy-child/functions.php
+++ wp-content/themes/blocksy-child/functions.php
@@ -9,20 +9,20 @@ function theme_enqueue_styles()
 
 function add_admin_link_to_menu($items, $args)
 {
-	// Vérifier si l'utilisateur est connecté
+
 	if (is_user_logged_in()) {
-		// Vérifier si le menu actuel est celui que vous voulez modifier
-		if ($args->theme_location == 'menu_mobile') { // Remplacez 'menu_mobile' par le nom de votre emplacement de menu
-			// Construire le lien vers le tableau de bord admin
+
+		if ($args->theme_location == 'menu_mobile') {
+
 			$admin_link = '<li class="menu-item"><a href="' . admin_url() . '">Admin</a></li>';
-			// Trouver la position où insérer le lien admin
+
 			$position = strpos($items, '</li>', strpos($items, 'menu-nous-rencontrer') + 1);
-			// Insérer le lien admin à la position trouvée
+
 			$items = substr_replace($items, $admin_link, $position, 0);
 		}
 	}
 	return $items;
 }
 
-// Ajouter le filtre pour exécuter la fonction lorsque le menu est généré
+
 add_filter('wp_nav_menu_items', 'add_admin_link_to_menu', 10, 2);
